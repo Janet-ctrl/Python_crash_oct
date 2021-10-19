a472f8d1369c01a4d956e7586b901f4749dc8819
Janet-ctrl <janet.eras@outlook.com>
2021-10-19 09:49:52 +0200

    project update

---
+++ Python crach course October/Alien invasion/settings.py
@@ -0,0 +1,11 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion."""
+
+	def __init__(self):
+		"""Initialize the game's settings."""
+		# Screen settings.
+		# Ship settings
+		self.ship_speed = 1.5
+		self.screen_width = 800
+		self.screen_height = 600
+		self.bg_color = (230, 230, 230)
